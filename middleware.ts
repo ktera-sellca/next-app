ab9512acad910382892c07d575f0c809b3ad2172
寺田恭 <kyo.terada@sellca-sellcar.com>
2025-05-05 00:08:15 +0900

    middleware お試し

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,12 @@
 import { NextResponse, type NextRequest } from "next/server";
+import { geolocation } from "@vercel/functions";
 
 export function middleware(req: NextRequest) {
-  const message = req.nextUrl.searchParams.get("message") || "hogehoge";
+  const { city, country } = geolocation(req);
   const res = NextResponse.next();
-  res.headers.append("x-message", message);
+
+  res.headers.append("x-city", city || "unknown");
+  res.headers.append("x-country", country || "unknown");
 
   return res;
 }
